7c38f4f9062bd32fcc61ed9f69fecce95439bb98
Jean Wang <jeanharmsen@yahoo.com>
2020-04-21 17:41:36 -0400

    add custom login error message

--- my-custom-wp-login.php
+++ my-custom-wp-login.php
@@ -14,3 +14,11 @@ add_action( 'login_enqueue_scripts', 'cwpl_login_stylesheet' );
 function cwpl_login_stylesheet() {
     wp_enqueue_style( 'cwpl-custom-stylesheet', plugin_dir_url( __FILE__ ) . 'login-styles.css' );
 }
+
+add_filter( 'login_errors', 'cwpl_login_errors' );
+/**
+ * Returns a custom login error message.
+ */
+function cwpl_login_errors(){
+  return 'The login info you entered was not correct!';
+}
