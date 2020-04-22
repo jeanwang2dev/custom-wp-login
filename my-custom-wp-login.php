d5896b66f24a0266591dbe52a7b8dd7dcfb45dd6
Jean Wang <jeanharmsen@yahoo.com>
2020-04-22 14:47:18 -0400

    remove shake effect on login page when login info is wrong

--- my-custom-wp-login.php
+++ my-custom-wp-login.php
@@ -22,3 +22,28 @@ add_filter( 'login_errors', 'cwpl_login_errors' );
 function cwpl_login_errors() {
   return 'The login info you entered was not correct!';
 }
+
+add_filter( 'login_headertext', 'cwpl_wp_login_title' );
+/**
+ * Change wp login title
+ */
+function cwpl_wp_login_title() {
+  return 'something new!';
+}
+
+add_filter( 'login_headerurl', 'cwpl_change_header_url' );
+/**
+ * Change Login page Header URL
+ */
+function cwpl_change_header_url() {
+    $url = 'https://jeanwang2dev.com';
+    return $url;
+}
+
+add_action( 'login_head', 'cwpl_remove_shake' );
+/**
+ * Remove login page shake effect when login info is wrong
+ */
+function cwpl_remove_shake() {
+  remove_action( 'login_head', 'wp_shake_js', 12 );
+}
